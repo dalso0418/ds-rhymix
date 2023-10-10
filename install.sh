f6085ec1d74c135c15449a5fbcfd1cd14112ad31
dalso0418 <dalso0418@gmail.com>
2023-10-10 23:07:01 +0900

    rhymix version update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd data
 echo "rhymix 최신버전 다운로드"
-wget https://rhymix.org/files/attach/releases/rhymix-2.0.24.zip
+wget https://rhymix.org/files/attach/releases/rhymix-2.1.7.zip
 unzip ./*
 echo "권한변경"
 chown -R 82:82 rhymix
